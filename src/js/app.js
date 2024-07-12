06c70728f2b6a5727f56b33e5b6344837b84efc5
Natalie Voronova <Nataless@yandex.ru>
2024-07-12 22:01:36 +0300

    saved changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
+import Character from './Character';
 import characters from './person';
 import Team from './Team';
 
 const team = new Team();
 team.addAll(...characters);
+
+console.log(`map.get('${this.members}') = ${Character}`);
